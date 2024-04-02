0ab4fbf50c3cc9e35e66e733d3e21923a48ed219
YusufToraman <yusuf.toraman@ug.bilkent.edu.tr>
2024-04-02 15:21:48 +0300

    init and destroy

--- mf.c
+++ mf.c
@@ -5,15 +5,74 @@
 #include <assert.h>
 #include <string.h>
 #include "mf.h"
+#include <string.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <semaphore.h>
 
-int mf_init()
-{
-    return (0);
+#define MF_SUCCESS 0 
+#define MF_ERROR -1
+
+typedef struct {
+    int mq_count; // Oluşturulan mesaj kuyruğu sayısı
+    // Diğer gerekli meta veriler...
+} SharedMemoryMeta;
+
+typedef struct {
+    char shmem_name[MAXFILENAME];
+    size_t shmem_size;
+    int max_msgs_in_queue;
+    int max_queues_in_shmem;
+} ConfigParams;
+
+//Global de yapabiliriz bence sorun yok ikisi de okey
+void *shmem = NULL;
+ConfigParams config;
+
+static ConfigParams read_config(const char* filename);
+static void* create_shared_memory(const char* name, size_t size);
+static int setup_synchronization_objects(SharedMemoryMeta* layout, ConfigParams config);
+
+int mf_init() {
+    config = read_config(CONFIG_FILENAME);
+    shmem = create_shared_memory(config.shmem_name, config.shmem_size);
+    if (!shmem) {
+        return MF_ERROR;
+    }
+    
+    //BU SHAREDMEMORYMETA KISMINA BİRLİKTE BAKALIM
+    SharedMemoryMeta* meta = (SharedMemoryMeta*)shmem;
+    meta->mq_count = 0;
+
+    // Senkronizasyon objelerini kur
+    if (setup_synchronization_objects(meta, config) != 0) {
+        munmap(shmem, config.shmem_size);
+        return MF_ERROR;
+    }
+
+    return MF_SUCCESS;
 }
 
-int mf_destroy()
-{
-    return (0);
+int mf_destroy() {
+    if (shmem != NULL) {
+        munmap(shmem, config.shmem_size);
+        shmem = NULL;
+    }
+    
+    if (config.shmem_name[0] != '\0') {
+        shm_unlink(config.shmem_name);
+        config.shmem_name[0] = '\0';  // İsmi temizle
+    }
+
+    // Semaforları kapat ve sil
+    for (int i = 0; i < config.max_queues_in_shmem; i++) {
+        char sem_name[256];
+        sprintf(sem_name, "/sem_queue_%d", i);
+        sem_unlink(sem_name);
+    }
+    
+    return MF_SUCCESS;
 }
 
 int mf_connect()
@@ -65,5 +124,67 @@ int mf_print()
     return (0);
 }
 
+//PRINT İLE DOĞRU OKUYOR MU DİYE TEST ET !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+static ConfigParams read_config(const char* filename) {
+    FILE* file = fopen(filename, "r");
+
+    strcpy(config.shmem_name, "/default_shmem");
+    config.shmem_size = MIN_SHMEMSIZE;
+    config.max_msgs_in_queue = 10;
+    config.max_queues_in_shmem = 5;
+    
+    if (!file) {
+        perror("Unable to open the config file");
+        return config;
+    }
+
+    char line[256];
+    while (fgets(line, sizeof(line), file)) {
+        if (line[0] == '#') continue;
+        if (sscanf(line, "SHMEM_NAME %s", config.shmem_name) == 1) continue;
+        if (sscanf(line, "SHMEM_SIZE %zu", &config.shmem_size) == 1) continue;
+        if (sscanf(line, "MAX_MSGS_IN_QUEUE %d", &config.max_msgs_in_queue) == 1) continue;
+        if (sscanf(line, "MAX_QUEUES_IN_SHMEM %d", &config.max_queues_in_shmem) == 1) continue;
+    }
+
+    printf("SHMEM_NAME: %s\nSHMEM_SIZE: %zu\nMAX_MSGS_IN_QUEUE: %d\nMAX_QUEUES_IN_SHMEM: %d", 
+    config.shmem_name, config.shmem_size, config.max_msgs_in_queue, config.max_queues_in_shmem);
+    fclose(file);
+    config.shmem_size *= 1024;
+    return config;
+}
+
+static void* create_shared_memory(const char* name, size_t size) {
+    int shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
+    if (shm_fd == -1) {
+        perror("shm_open");
+        return NULL;
+    }
+    if (ftruncate(shm_fd, size) == -1) {
+        perror("ftruncate");
+        close(shm_fd);
+        return NULL;
+    }
+    void* addr = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (addr == MAP_FAILED) {
+        perror("mmap");
+        close(shm_fd);
+        return NULL;
+    }
+    return addr;
+}
+
+static int setup_synchronization_objects(SharedMemoryMeta* layout, ConfigParams config) {
+    for (int i = 0; i < config.max_queues_in_shmem; i++) {
+        char sem_name[256];
+        sprintf(sem_name, "/sem_queue_%d", i);
+        sem_t* sem = sem_open(sem_name, O_CREAT, 0666, 1);
+        if (sem == SEM_FAILED) {
+            perror("sem_open");
+            return -1;
+        }
+    }
+    return 0;
+}
 
 
--- mfserver.c
+++ mfserver.c
@@ -5,27 +5,31 @@
 #include <assert.h>
 #include <string.h>
 #include "mf.h"
-
+#include <signal.h>
 
 // write the signal handler function
 // it will call mf_destroy()
 //
-
-int
-main(int argc, char *argv[])
-{
-    
-    printf ("mfserver pid=%d\n", (int) getpid());
-    
-    // register the signal handler function
-    
-    
-    mf_init(); // will read the config file
-    
-    while (1)
-        sleep(1000);
-    
+void signal_handler(int sig) {
+    printf("Signal %d received, cleaning up...\n", sig);
+    mf_destroy();
     exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+    printf("mfserver pid=%d\n", (int) getpid());
+
+    signal(SIGINT, signal_handler);
+    signal(SIGTERM, signal_handler);
+    
+    mf_init();
+    
+    while (1) {
+        sleep(1000);
+    }
+
+    return 0;
 }
 
 
