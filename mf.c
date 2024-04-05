1abd513357b51b4e9df6ed8708eb9ca9fd43dba3
dilaramandiraci <dilaramandiracii@gmail.com>
2024-04-05 22:07:39 +0300

    hole manager and mf_create

--- mf.c
+++ mf.c
@@ -16,6 +16,16 @@
 #define MF_ERROR -1
 
 typedef struct {
+    size_t start; // Offset to the start of the hole
+    size_t size;  // Size of the hole
+    size_t next;  // Offset to the next hole, 0 if this is the last
+} Hole;
+
+typedef struct {
+    size_t firstHoleOffset; // Offset of the first hole in the list, 0 if no holes
+} HoleManager;
+
+typedef struct {
     char shmem_name[MAXFILENAME];
     size_t shmem_size;
     int max_msgs_in_queue;
@@ -24,14 +34,76 @@ typedef struct {
 
 typedef struct {
     int mq_count;
+    int unique_id;
     ConfigParams config;
-    //hole_manager
+    HoleManager holeManager;
 } FixedPortion;
 
 void *shmem = NULL;
 
 static ConfigParams read_config(const char* filename);
 static void* create_shared_memory(const char* name, size_t size);
+
+size_t find_free_space_for_queue(FixedPortion* fixedPortion, size_t requestedSize) {
+    size_t prevOffset = 0;
+    size_t currentOffset = fixedPortion->holeManager.firstHoleOffset;
+    
+    while (currentOffset != 0) {
+        Hole* hole = (Hole*)((char*)shmem + currentOffset);
+        
+        if (hole->size >= requestedSize) {
+            // Check if the hole is large enough to be split
+            if (hole->size > requestedSize + sizeof(Hole)) {
+                size_t newHoleOffset = currentOffset + sizeof(Hole) + requestedSize;
+                Hole* newHole = (Hole*)((char*)shmem + newHoleOffset);
+                newHole->start = hole->start + requestedSize;
+                newHole->size = hole->size - requestedSize - sizeof(Hole);
+                newHole->next = hole->next;
+                
+                hole->size = requestedSize; // Adjust the current hole size
+                
+                if (prevOffset == 0) {
+                    // This was the first hole, update the manager
+                    fixedPortion->holeManager.firstHoleOffset = newHoleOffset;
+                } else {
+                    // Link the previous hole to the new hole
+                    Hole* prevHole = (Hole*)((char*)shmem + prevOffset);
+                    prevHole->next = newHoleOffset;
+                }
+                return hole->start; // Return the start of the allocated space
+            } else {
+                // Use the hole as-is, remove it from the list
+                if (prevOffset == 0) {
+                    fixedPortion->holeManager.firstHoleOffset = hole->next;
+                } else {
+                    Hole* prevHole = (Hole*)((char*)shmem + prevOffset);
+                    prevHole->next = hole->next;
+                }
+                return hole->start;
+            }
+        }
+        
+        prevOffset = currentOffset;
+        currentOffset = hole->next;
+    }
+    
+    return (size_t)-1; // Failed to find a suitable space
+}
+
+
+void initialize_hole_manager(FixedPortion* fixedPortion, size_t totalShmemSize) {
+    size_t startOfFreeSpace = sizeof(FixedPortion);
+    size_t firstHoleOffset = startOfFreeSpace;
+
+    Hole* firstHole = (Hole*)((void*)shmem + firstHoleOffset);
+
+    firstHole->start = firstHoleOffset; // Start of the hole in offset terms
+    firstHole->size = totalShmemSize - startOfFreeSpace; // Size of the hole
+    firstHole->next = 0; // Indicate that this is the only hole for now
+
+    fixedPortion->holeManager.firstHoleOffset = firstHoleOffset;
+}
+
 
 int mf_init() {
     ConfigParams config = read_config(CONFIG_FILENAME);
@@ -43,10 +115,9 @@ int mf_init() {
     
     FixedPortion* fixedPortion = (FixedPortion*)shmem;
     fixedPortion->config = config; 
-    fixedPortion->mq_count = 0;   
-    
-    // If using a hole manager or similar, initialize it here
-    // initialize_hole_manager(&fixedPortion->hole_manager); // Adjust based on your implementation
+    fixedPortion->mq_count = 0; 
+    fixedPortion->unique_id = 1;  
+    initialize_hole_manager(&fixedPortion, config.shmem_size);
     
     return MF_SUCCESS;
 }
@@ -105,10 +176,47 @@ int mf_disconnect() {
     return MF_SUCCESS;
 }
 
-int mf_create(char *mqname, int mqsize)
-{
-    return (0);
+int mf_create(char *mqname, int mqsize) {
+
+    FixedPortion* fixedPortion = (FixedPortion*)shmem;
+    size_t requiredSize = sizeof(MessageQueueHeader) + mqsize;
+    
+    size_t offset = find_free_space_for_queue(fixedPortion->config.shmem_size, requiredSize);
+    if (offset == (size_t)-1) {
+        return MF_ERROR;
+    }
+    
+    MessageQueueHeader* mqHeader = (MessageQueueHeader*)((char*)shmem + offset);
+    
+    strncpy(mqHeader->mq_name, mqname, MAX_MQNAMESIZE);
+    mqHeader->mq_name[MAX_MQNAMESIZE - 1] = '\0'; 
+    //bu isim kopyalama böyle oluyomus 
+
+    mqHeader->start_pos_of_queue = offset + sizeof(MessageQueueHeader); // Headerin arkasında queue olarak kullanacağımız yer
+    mqHeader->end_pos_of_queue = mqHeader->start_pos_of_queue + mqsize; // bu da başlangıç + data size yani mqsize
+    mqHeader->mq_data_size = mqsize; 
+    mqHeader->in = 0;
+    mqHeader->out = 0; 
+    mqHeader->max_messages_allowed = fixedPortion->config.max_msgs_in_queue; 
+    mqHeader->total_message_no = 0;
+    mqHeader->qid = fixedPortion->unique_id++;
+
+    char semName[64]; // Adjust size as necessary
+    sprintf(semName, "/mq_%d_sem", mqHeader->qid); // Unique semaphore name based on the queue ID
+
+    // Create and initialize the named semaphore
+    mqHeader->sem = sem_open(semName, O_CREAT | O_EXCL, 0644, 1); // Initial value of 1
+    if (mqHeader->sem == SEM_FAILED) {
+        perror("Failed to open semaphore");
+        // Handle semaphore creation failure, possibly by cleaning up already allocated resources
+        return MF_ERROR;
+    }
+
+    fixedPortion->mq_count++;
+    
+    return mqHeader->qid;
 }
+
 
 int mf_remove(char *mqname)
 {
